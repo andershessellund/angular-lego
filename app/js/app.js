54c7f94c77f6879d273265905f5d5c5e20f9a3dd
Anders Hessellund Jensen <ahj@trifork.com>
2013-04-18 17:42:45 +0200

    step8: XXX items left skal vise korrekt antal.

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function TodoCtrl($scope) {
+function TodoCtrl($scope, filterFilter) {
     $scope.todos = [
         { completed: true, title: 'Vis liste af eksisterende TODOs (en mock-liste til at starte med).' },
         { completed: true, title: 'Markér TODO som færdig ved at klikke på flueben.' },
@@ -9,13 +9,17 @@ function TodoCtrl($scope) {
         { completed: true, title: 'Skriv ny TODO ved at angive tekst og trykke enter.' },
         { completed: true, title: 'Redigér eksisterende TODO ved at dobbeltklikke på teksten og redigere.' },
         { completed: true, title: 'Fjern eksisterende TODO ved at trykke på krydset.' },
-        { completed: false, title: '"XXX items left" skal vise korrekt antal.' },
+        { completed: true, title: '"XXX items left" skal vise korrekt antal.' },
         { completed: false, title: '"Clear completed (XXX)" skal rydde færdige TODOs og vise korrekt antal.' },
         { completed: false, title: 'Mulighed for at markere/fravælge alle.' },
         { completed: false, title: 'Vis kun "main"-sektion hvis der overhovedet er nogen TODOs.' },
         { completed: false, title: 'Vis kun "footer"-sektion hvis der overhovedet er nogen TODOs.' },
         { completed: false, title: 'Gem listen af TODOs i localStorage.' }
     ];
+
+    $scope.$watch('todos', function() {
+        $scope.remainingCount = filterFilter($scope.todos, $scope.filters.active).length;
+    }, true);
 
     $scope.filters = {
         all: null,
