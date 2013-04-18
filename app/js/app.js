af57c32ba1d55a7884bf147ab864fcace4b19890
Anders Hessellund Jensen <ahj@trifork.com>
2013-04-18 16:58:30 +0200

    step2

--- app/js/app.js
+++ app/js/app.js
@@ -2,19 +2,19 @@
 
 function TodoCtrl($scope) {
     $scope.todos = [
-        { title: 'Vis liste af eksisterende TODOs (en mock-liste til at starte med).' },
-        { title: 'Markér TODO som færdig ved at klikke på flueben.' },
-        { title: '"All"/"Active"/"Completed" skal vise relevante TODOs.' },
-        { title: 'Den aktuelle side ("All"/"Active"/"Completed") skal fremhæves.' },
-        { title: 'Skriv ny TODO ved at angive tekst og trykke enter.' },
-        { title: 'Redigér eksisterende TODO ved at dobbeltklikke på teksten og redigere.' },
-        { title: 'Fjern eksisterende TODO ved at trykke på krydset.' },
-        { title: '"XXX items left" skal vise korrekt antal.' },
-        { title: '"Clear completed (XXX)" skal rydde færdige TODOs og vise korrekt antal.' },
-        { title: 'Mulighed for at markere/fravælge alle.' },
-        { title: 'Vis kun "main"-sektion hvis der overhovedet er nogen TODOs.' },
-        { title: 'Vis kun "footer"-sektion hvis der overhovedet er nogen TODOs.' },
-        { title: 'Gem listen af TODOs i localStorage.' }
+        { completed: true, title: 'Vis liste af eksisterende TODOs (en mock-liste til at starte med).' },
+        { completed: true, title: 'Markér TODO som færdig ved at klikke på flueben.' },
+        { completed: false, title: '"All"/"Active"/"Completed" skal vise relevante TODOs.' },
+        { completed: false, title: 'Den aktuelle side ("All"/"Active"/"Completed") skal fremhæves.' },
+        { completed: false, title: 'Skriv ny TODO ved at angive tekst og trykke enter.' },
+        { completed: false, title: 'Redigér eksisterende TODO ved at dobbeltklikke på teksten og redigere.' },
+        { completed: false, title: 'Fjern eksisterende TODO ved at trykke på krydset.' },
+        { completed: false, title: '"XXX items left" skal vise korrekt antal.' },
+        { completed: false, title: '"Clear completed (XXX)" skal rydde færdige TODOs og vise korrekt antal.' },
+        { completed: false, title: 'Mulighed for at markere/fravælge alle.' },
+        { completed: false, title: 'Vis kun "main"-sektion hvis der overhovedet er nogen TODOs.' },
+        { completed: false, title: 'Vis kun "footer"-sektion hvis der overhovedet er nogen TODOs.' },
+        { completed: false, title: 'Gem listen af TODOs i localStorage.' }
     ];
 }
 
